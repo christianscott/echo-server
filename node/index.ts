d694af706fbb37ad6c92168e4010b2892eed5da3
Christian Scott <christian.s@canva.com>
2019-03-19 20:28:44 +1100

    add node

---
+++ node/index.ts
@@ -0,0 +1,21 @@
+import * as net from 'net';
+
+const server = net.createServer((connection) => {
+  connection.setEncoding('utf8');
+
+  let buffer: string[] = [];
+  connection.on('data', (data: string) => {
+    const [current, ...remainingLines] = data.split('\n');
+    if (remainingLines.length > 0) {
+      connection.write([...buffer, current, '\n'].join(''));
+      buffer.push(...remainingLines);
+    } else {
+      buffer.push(current);
+    }
+  });
+});
+
+server.listen(3000, () => {
+  console.log('server listening on :3000');
+});
+
